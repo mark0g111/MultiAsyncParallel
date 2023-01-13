013909942370fd9cce83b0f510ce5d1c745f81e4
mark0g111 <9154547225@mail.ru>
2023-01-13 14:50:27 +0300

    Solution of the problem

--- multithreading/02_foreground_background_threads.py
+++ multithreading/02_foreground_background_threads.py
@@ -1,9 +1,13 @@
+import threading
+
 from count_three_sum import read_ints, count_three_sum
 
 if __name__ == '__main__':
     print('Started main')
 
     ints = read_ints(r'..\data\1Kints.txt')
-    count_three_sum(ints)
 
+    t1 = threading.Thread(target=count_three_sum, args=(ints,), daemon=True)
+    t1.start()
+    t1.join()
     print('Ended main')
