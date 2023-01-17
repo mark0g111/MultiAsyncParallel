80bcf89b85cf2bb9054b444a4ac3c19ea19b96bc
mark0g111 <9154547225@mail.ru>
2023-01-17 09:46:10 +0300

    barrier demo

---
+++ multithreading/13_barrier.py
@@ -0,0 +1,35 @@
+import datetime
+import random
+import threading
+import time
+
+
+class HorseRace:
+    def __init__(self):
+        self.barrier = threading.Barrier(4)
+        self.horses = ['Artax', 'Frankel', 'Bucephalus', 'Barton']
+
+    def lead(self):
+        horse = self.horses.pop()
+        time.sleep(random.randrange(1, 5))
+        print(f'\n{horse} reached the barrier at: {datetime.datetime.now()}')
+        self.barrier.wait()
+
+        time.sleep(random.randrange(1, 5))
+        print(f'\n{horse} started at: {datetime.datetime.now()}')
+
+        time.sleep(random.randrange(1, 5))
+        print(f'\n{horse} finished at: {datetime.datetime.now()}')
+
+        self.barrier.wait()
+        print(f'{horse} went sleeping')
+
+
+if __name__ == '__main__':
+
+    print('Race preparation')
+
+    race = HorseRace()
+    for _ in range(4):
+        thread = threading.Thread(target=race.lead)
+        thread.start()
